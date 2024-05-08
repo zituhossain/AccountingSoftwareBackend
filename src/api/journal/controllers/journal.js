f6e2e48affc2b7160992dc94277cc8b56d7263b9
zituhossain5 <zituhossain@gmail.com>
2024-05-08 16:54:39 +0600

    bring created_at in query for income report

--- src/api/journal/controllers/journal.js
+++ src/api/journal/controllers/journal.js
@@ -9,22 +9,33 @@ module.exports = createCoreController("api::journal.journal", ({ strapi }) => ({
   // Custom controller action to find journals by multiple credit account ids and between two dates
   async findRevenueJournalByRevenueIdAndDate(ctx) {
     const { revenueId } = ctx.params;
+    const { startDate, endDate } = ctx.query;
 
     console.log("revenueId:", revenueId);
 
     try {
-      const knex = strapi.db.connection; // Accessing the knex instance directly
+      const knex = strapi.db.connection;
 
-      const journals = await knex.raw(`
+      const journals = await knex.raw(
+        `
         SELECT journals.*, individual_accounts.*, journals_credit_account_links.*
         FROM journals
         JOIN journals_credit_account_links ON journals.id = journals_credit_account_links.journal_id
         JOIN individual_accounts ON journals_credit_account_links.individual_account_id = individual_accounts.id
-        WHERE individual_accounts.id IN (${revenueId})
-        `);
+        WHERE individual_accounts.id IN (?)
+        AND journals.created_at >= ?
+        AND journals.created_at <= ?
+      `,
+        [revenueId.split(","), new Date(startDate), new Date(endDate)]
+      );
+
       // AND journals.date BETWEEN '2024-05-01' AND '2024-05-07'
 
       console.log("journals:", journals[0]);
+
+      console.log("Received revenueId:", revenueId);
+      console.log("Received startDate:", startDate);
+      console.log("Received endDate:", endDate);
 
       return journals[0];
     } catch (err) {
@@ -35,19 +46,24 @@ module.exports = createCoreController("api::journal.journal", ({ strapi }) => ({
 
   async findExpenseJournalByExpenseIdAndDate(ctx) {
     const { expenseIds } = ctx.params;
-
+    const { startDate, endDate } = ctx.query;
     console.log("expenseIds:", expenseIds);
 
     try {
-      const knex = strapi.db.connection; // Accessing the knex instance directly
+      const knex = strapi.db.connection;
 
-      const journals = await knex.raw(`
+      const journals = await knex.raw(
+        `
         SELECT journals.*, individual_accounts.*, journals_debit_account_links.*
         FROM journals
         JOIN journals_debit_account_links ON journals.id = journals_debit_account_links.journal_id
         JOIN individual_accounts ON journals_debit_account_links.individual_account_id = individual_accounts.id
-        WHERE individual_accounts.id IN (${expenseIds})
-        `);
+        WHERE individual_accounts.id IN (?)
+        AND journals.created_at >= ?
+        AND journals.created_at <= ?
+      `,
+        [expenseIds.split(","), new Date(startDate), new Date(endDate)]
+      );
       // AND journals.date BETWEEN '2024-05-01' AND '2024-05-07'
 
       console.log("journals:", journals[0]);
